9e850e2c7a67c25401b3fcd4f7b7b0dc5d13eb8d
Aleix Renom <aleixrenom@gmail.com>
2020-03-05 14:32:11 +0200

    first working version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div>
+		<BrowserRouter>
+		<div>
+			<Link to="/">Home</Link>{' '}
+			<Link to="/about">About</Link>{' '}
+			<Link to="/contact">Contact</Link>{' '}
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/about" component={About} />
+				<Route path="/contact" component={Contact} />
+				<Route render={() => <h1>Page not found</h1>} />
+			</Switch>
+		</div>
+		</BrowserRouter>
+	</div>
   );
 }
 
